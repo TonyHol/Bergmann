06714597e0f85214b567c209fa050c0a7062cea1
TonyHol <76104666+TonyHol@users.noreply.github.com>
2021-02-28 16:55:58 +0200

    initial

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+
+export const Navbar = () => {
+  const history = useHistory()
+  const auth = useContext(AuthContext)
+
+  const logoutHandler = (event) => {
+    event.preventDefault()
+    auth.logout()
+    history.push('/')
+  }
+  return (
+    <nav>
+      <div className="nav-wrapper blue darken-1" style={{ padding: '0 2rem' }}>
+        <a href="/!" className="brand-logo">
+          Links
+        </a>
+        <a href="/" data-target="mobile-demo" className="sidenav-trigger">
+          <i className="material-icons">menu</i>
+        </a>
+        <ul className="right hide-on-med-and-down">
+          <li>
+            <NavLink to="/create">Create</NavLink>
+          </li>
+          <li>
+            <NavLink to="/links">links</NavLink>
+          </li>
+          <li>
+            <a href="/" onClick={logoutHandler}>
+              Log out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
